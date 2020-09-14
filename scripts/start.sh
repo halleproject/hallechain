a38de1873806aae6fa1343c43aa09f78f3a2c8d8
wade <wade-liwei@icloud.com>
2020-09-14 11:26:46 +0800

    fix script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,11 +2,11 @@
 
 if [ -n "$1" ]
 then
-	halled start   --pruning=nothing   --minimum-gas-prices  5.0uhale   --home  $1  --log_level "main:info,state:info,mempool:info"  --rpc.laddr "tcp://0.0.0.0:26657" &
+	./halled start   --pruning=nothing   --minimum-gas-prices  5.0uhale   --home  $1  --log_level "main:info,state:info,mempool:info"  --rpc.laddr "tcp://0.0.0.0:26657" &
 
 	sleep 5
 
-  hallecli rest-server --laddr tcp://0.0.0.0:8545 --unlock-key mykey0 --chain-id 200812 --trace --home clicfg/ --unsafe-cors=true &
+        ./hallecli rest-server --laddr tcp://0.0.0.0:8545 --unlock-key mykey0 --chain-id 200812 --trace --home clicfg/ --unsafe-cors=true &
   tail -f /dev/null
 
 	fi
